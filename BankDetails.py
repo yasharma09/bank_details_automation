e18c746b883a28de08b3a0173b0e1e6cc05f7bf9
Yash Sharma <121340980+yasharma09@users.noreply.github.com>
2025-01-23 00:52:44 +0530

    Update BankDetails.py

--- BankDetails.py
+++ BankDetails.py
@@ -9,10 +9,6 @@ st.title("Bank Details Working")
 
 uploaded_file1 = st.file_uploader("Upload the input file", type=["csv","xlsx"])
 
-
-
-
-
 def processedFile(uploaded_file1):
     if uploaded_file1: 
         bank_detail_df=pd.read_excel(uploaded_file1)
@@ -20,23 +16,18 @@ def processedFile(uploaded_file1):
 
 
 
-        con = pymysql.connect(host ='easy-prod-db-replica-new.cpmomzsxd8rm.ap-south-1.rds.amazonaws.com',user = 'YashSharma',passwd = 'Vrv6XukaKb8ECUK',port=3306)
+        con = pymysql.connect(host ='easy-prod-db-replica-new.cpmomzsxd8rm.ap-south-1.rds.amazonaws.com',user = 'YashSharma',password = 'Vrv6XukaKb8ECUK',port=3306)
         con.connect()
         refund_details1 = pd.read_sql('''select  distinct a.bank_account_number,a.customer_id,  a.bank_account_holder, 
         a.bank_code, b.ifsc_code 
         from payment_service.autopay_subscriptions a join loan_service.bank_details b
-        on a.bank_account_number = b.bank_account_no
-        where a.subscription_status = 'ACTIVE'
-        and a.customer_id in {}
-        '''.format(tuple(bank_detail_df['customer_id'])),con)
+        on a.bank_account_number = b.bank_account_no where a.subscription_status = 'ACTIVE'
+        and a.customer_id in {}'''.format(tuple(bank_detail_df['customer_id'])),con)
 
-        con.close()
-        refund_details1.shape
+        # con.close()
         bank_detail_df['customer_id']=bank_detail_df.customer_id.astype(int)
         refund_details1['customer_id']=refund_details1.customer_id.astype(int)
         final_df = pd.merge(bank_detail_df, refund_details1, on='customer_id', how='left')
-
-
         file_path = "Bank_details_result.xlsx"
         final_df.to_excel(file_path, index=False)
         st.write(f"Final File saved in {file_path}")
